986688863cdfc67124ae07a93fe65eeeb0b75c5a
Cleon Pinto <cleon@riversun.co.nz>
2025-01-20 23:26:40 +1300

    wip

--- src/routes/(main)/timelog/+page.svelte
+++ src/routes/(main)/timelog/+page.svelte
@@ -3,6 +3,7 @@
     import TaskList from "$lib/TaskList.svelte";
     import { setTaskRepoContext, TaskRepo } from "$lib/TaskRepo.svelte";
     import TimeLogger from "$lib/TimeLogger.svelte";
+    import { TaskState } from "$lib/Types.svelte";
     import { onDestroy, onMount } from "svelte";
     
     const taskRepo = setTaskRepoContext( new TaskRepo() );
@@ -15,10 +16,39 @@
     onDestroy(() =>{
         taskController.stop();
     })
+
+    let tasksRunning = $derived( taskController.fetchTasksByState( [TaskState.Running] ) );
+    let tasksPaused = $derived( taskController.fetchTasksByState( [TaskState.Paused] ) );
+    let tasksStopped = $derived( taskController.fetchTasksByState( [TaskState.Stopped] ) );
+    let tasksArchived = $derived( taskController.fetchTasksByState( [TaskState.Archived] ) );
+
 </script>
 
 <h1>Time Log</h1>
 
 <TimeLogger/>
 
-<TaskList/>
+<section>
+    <summary>In Progress ({tasksRunning.length})</summary>
+    <TaskList tasks={tasksRunning}/>
+</section>
+
+<section>
+    <summary>Paused ({tasksPaused.length})</summary>
+    <TaskList tasks={tasksPaused}/>
+</section>
+
+<section>
+    <details>
+        <summary>Stopped ({tasksStopped.length})</summary>
+        <TaskList tasks={tasksStopped}/>
+    </details>
+</section>
+
+
+<section>
+    <details>
+        <summary>Archived ({tasksArchived.length})</summary>
+        <TaskList tasks={tasksArchived}/>
+    </details>
+</section>
