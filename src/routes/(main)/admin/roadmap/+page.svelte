596084836be8836f64d720a8556b8c8ed0bb2b09
Cleon Pinto <cleon@riversun.co.nz>
2025-01-24 01:02:05 +1300

    update roadmap

--- src/routes/(main)/admin/roadmap/+page.svelte
+++ src/routes/(main)/admin/roadmap/+page.svelte
@@ -7,11 +7,6 @@
 		<br>- See: https://stackoverflow.com/questions/13335967/how-can-you-import-export-the-entire-localstorage
 		<br>- Use btoa and atob for encoding to protect from JSON double quoting
 	</li>
-	<li>Fix Duration dark mode</li>
-	<li>
-		Better icons
-		<br>- https://lucide.dev/guide/packages/lucide-svelte
-	</li>
 	<li>Easy logo selection</li>
 	<li>Task description suggestions
 		<br>- datalist (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist)
@@ -21,7 +16,11 @@
 
 <h2>Completed</h2>
 <ul>
-	<li>2025-01-23 Initial release</li>
+	<li>
+		2025-01-23
+		<br>- Better icons
+		<br>- Fix Duration dark mode
+	</li>
 </ul>
 
 <style>
