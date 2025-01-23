cbe7a73cb3d747fea608561d2fbf0c9ae8da8780
Cleon Pinto <cleon@riversun.co.nz>
2025-01-24 05:39:19 +1300

    ui tweaks

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -9,10 +9,29 @@
 	</style>
 </svelte:head>
 
-<script>
+<script lang="ts">
+    import { goto } from '$app/navigation';
     import { BackupService } from '$lib/services/BackupAndRestore';
+    import Icon from '$lib/views/Icon.svelte';
+    import { faBars } from '@fortawesome/free-solid-svg-icons';
+    import { preventDefault } from 'svelte/legacy';
 
 	let { children } = $props();
+
+	const memuItems:{ label:string, url:string }[] = [
+		{ label:"Time Log", url:"/timelog/" },
+		{ label:"Invoice Builder", url:"/invoices/" },
+		{ label:"Settings", url:"/admin/settings/" },
+		{ label:"Backup & Restore", url:"/admin/backup/" },
+		{ label:"Roadmap", url:"/admin/roadmap/" },
+	]
+	
+
+	let elMenu:HTMLElement|undefined = $state();
+
+	function onMenuItemClick(){
+		elMenu?.attributes.removeNamedItem("open")
+	}
 
 	const backupSvr = new BackupService();
 </script>
@@ -22,8 +41,34 @@
 		<li>
 			<strong style="letter-spacing: 0.3rem;">
 				[Timley]
+			</strong> 
+		</li>
+	</ul>
+	<ul>
+		<li><button class="outline" onclick={() => backupSvr.downloadBackupFile()}>Backup</button></li>
+		<li>
+			<details class="dropdown" bind:this={elMenu}>
+				<summary style="width: 18ch;">Menu</summary>
+				<ul>
+					{#each memuItems as mi}
+						<li><a href="{mi.url}" onclick="{onMenuItemClick}">{mi.label}</a></li>
+					{/each}
+
+				</ul>
+			</details>
+		</li>
+	</ul>
+  </nav>
+
+<!-- <nav class="container">
+	<ul>
+		<li>
+			<strong style="letter-spacing: 0.1rem;">
+				[Timley]
 			</strong>
 		</li>
+	</ul>
+	<ul>
 		<li><a href="/">Home</a></li>
 		<li><a href="/timelog/">Time Log</a></li>
 		<li><a href="/invoices/">Invoice Builder</a></li>
@@ -32,7 +77,6 @@
 		<li><button onclick={() => backupSvr.downloadBackupFile()}>Backup</button></li>
 		<li>
 			<details class="dropdown">
-				<summary>Admin</summary>
 				<ul>
 					<li><a href="/admin/settings/">Settings</a></li>
 					<li><a href="/admin/backup">Backup &amp; Restore</a></li>
@@ -41,7 +85,8 @@
 			</details>
 		</li>
 	</ul>
-</nav>
+	<Icon icon={faBars}/>
+</nav> -->
 <div class="container">
 	{@render children()}
 </div>
