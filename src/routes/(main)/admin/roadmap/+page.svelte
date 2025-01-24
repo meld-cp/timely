4b8570d25d9ad0e367147c12fa5c5d2e46a0905a
Cleon Pinto <cleon@riversun.co.nz>
2025-01-25 03:13:24 +1300

    ignore 0 qty lines when building invoices

--- src/routes/(main)/admin/roadmap/+page.svelte
+++ src/routes/(main)/admin/roadmap/+page.svelte
@@ -2,9 +2,6 @@
 
 <h3>To Do</h3>
 <ul>
-	<li>Task description suggestions
-		<br>- datalist (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist)
-	</li>
 	<li>Ability to edit Invoices
 	</li>
 </ul>
@@ -13,7 +10,10 @@
 <h3>Completed</h3>
 <ul>
 	<li>2025-01-25
+		<br>- Ignore 0 quantity lines when building invoices
+		<br>- Settings - add default currency
 		<br>- Easy logo selection
+		<br>- Task description suggestions
 	</li>
 	<li>
 		2025-01-23
