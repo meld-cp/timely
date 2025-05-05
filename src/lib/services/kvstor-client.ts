21db15a9e4a3618141a2486a949edc6b9480e669
meld-cp <>
2025-05-06 07:34:50 +1200

    add data page, update kv service and client

--- src/lib/services/kvstor-client.ts
+++ src/lib/services/kvstor-client.ts
@@ -5,21 +5,40 @@ export class KvStorClient {
 		public appId: string,
 	) {}
 
-	private buildUrl( bucketId: string, key: string ):URL {
+	private buildItemUrl( bucketId: string, key: string ):URL {
 		return new URL(`${this.remote}/${this.userId}/${this.appId}/${bucketId}/${key}`);
 	}
 
+	private buildListUrl( bucketId: string ):URL {
+		return new URL(`${this.remote}/${this.userId}/${this.appId}/${bucketId}/`);
+	}
+
+	public async getItemList( bucketId: string ):Promise<{ key:string, value:string }[]> {
+		const url = this.buildListUrl( bucketId);
+		//console.debug(url);
+		const res = await fetch(url);
+		const json = await res.json();
+		
+		
+		return json as { key:string, value:string }[];
+	}
+
 	public async getItem( bucketId: string, key: string ):Promise<string> {
-		const url = this.buildUrl( bucketId, key );
+		const url = this.buildItemUrl( bucketId, key );
 		const res = await fetch(url);
 		return await res.text();
 	}
 
 	public async setItem( bucketId: string, key: string, value: string ):Promise<void> {
-		const url = this.buildUrl( bucketId, key);
-		const res = await fetch(url, {
+		const url = this.buildItemUrl( bucketId, key);
+		const _ = await fetch(url, {
 			method: "POST",
 			body: value,
 		});
+	}
+
+	public async delItem( bucketId: string, key: string ):Promise<void> {
+		const url = this.buildItemUrl( bucketId, key );
+		const _ = await fetch(url,{ method: "DELETE" });
 	}
 }
