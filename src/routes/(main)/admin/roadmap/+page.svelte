00ae11b32d414528ad89e29a0f43d9b212262fbe
meld-cp <>
2025-05-06 07:34:47 +1200

    update roadmap

--- src/routes/(main)/admin/roadmap/+page.svelte
+++ src/routes/(main)/admin/roadmap/+page.svelte
@@ -2,18 +2,27 @@
 
 <h3>To Do</h3>
 <ul>
-	<li>Ability to edit Invoices
+	<li>
+		Next
+		<br>- Invoices - Auto save draft invoices
+	</li>
+	<li>
+		Later
+		<br>- Invoices - Ability to edit
 	</li>
 </ul>
 
 
 <h3>Completed</h3>
 <ul>
-	<li>2025-01-25
-		<br>- Ignore 0 quantity lines when building invoices
+	<li>
+		2025-01-25
+		<br>- Invoices - sort and renumber lines
+		<br>- Invoices - delete line
+		<br>- Invoices - Ignore 0 quantity lines when building invoices
 		<br>- Settings - add default currency
-		<br>- Easy logo selection
-		<br>- Task description suggestions
+		<br>- Settings - Easy logo selection
+		<br>- Time Log - Task description suggestions
 	</li>
 	<li>
 		2025-01-23
