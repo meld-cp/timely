f1946fb8131038079631b7522d20fb5c0c136820
meld-cp <>
2025-05-06 07:34:57 +1200

    add page visible detector

---
+++ src/lib/views/VisibilityDetector.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+
+	onMount(() => {
+		document.addEventListener( "visibilitychange", handleVisibilityChange );
+	});
+
+	onDestroy(() => {
+		document.removeEventListener( "visibilitychange", handleVisibilityChange );
+	});
+
+	let {
+		onPageVisibilityChanged
+	}:{
+		onPageVisibilityChanged:( visible:boolean ) => void
+	} = $props();
+
+	function handleVisibilityChange() {
+		onPageVisibilityChanged( document.visibilityState === "visible" );
+	}
+</script>
